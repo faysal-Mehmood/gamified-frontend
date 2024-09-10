2456099d4f9683077c6e5fe7f3d346e2a1c305d4
fasyal-ext <faysal.ext@ahdustechnology.com>
2024-09-10 22:56:51 +0500

    fix more on work with us section

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,9 @@
 import Image from "next/image";
-import WorkWithUsSection from "@/views/shared/WorkWithUs";
+import WorkWithUsSection from "@/views/sections/WorkWithUs";
 export default function Home() {
-  return <main>{/* <WorkWithUsSection /> */}</main>;
+  return (
+    <main>
+      <WorkWithUsSection />
+    </main>
+  );
 }
--- src/views/sections/WorkWithUs.js
+++ src/views/sections/WorkWithUs.js
@@ -5,7 +5,10 @@ const WorkWithUsSection = () => {
     <section className="flex flex-col md:flex-row items-center justify-between bg-gradient-to-r from-black via-blue-900 to-black py-12 px-6 md:px-16 lg:px-24">
       {/* Text Section */}
       <div className="md:w-1/2 text-center md:text-left space-y-6">
-        <h2 className="text lg:text-5xl font-bold text-foreground">
+        <h2
+          className="text-[90px] lg:text-5xl font-bold bg-gradient-to-r from-[#4D89F0] to-[#9184EA] 
+            bg-clip-text text-transparent"
+        >
           WORK WITH US
         </h2>
         <p className="text-gray-300 text-lg lg:text-xl">
@@ -26,7 +29,7 @@ const WorkWithUsSection = () => {
       <div className="mt-10 md:mt-0 md:w-1/2 flex justify-center">
         <div className="relative w-64 h-64 lg:w-96 lg:h-96">
           <Image
-            src="/image.png" // Replace with the correct path to your image
+            src="/images/hands1.svg" // Replace with the correct path to your image
             alt="Shaking Hands"
             layout="fill"
             objectFit="contain"
