68a83e4e5f781d7c2ca984e9d032fe7fee39c453
faysal-Mehmood <faysal.mehmood101@gmail.com>
2024-09-10 17:20:03 +0500

    inital release

--- src/app/page.js
+++ src/app/page.js
@@ -80,21 +80,6 @@ export default function Home() {
           />
           Examples
         </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
       </footer>
     </div>
   );
