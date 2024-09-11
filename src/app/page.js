8efad8f137546cd64bc31d25a7626a3aa402bebc
faysal-Mehmood <faysal.mehmood101@gmail.com>
2024-09-11 20:13:12 +0500

    add our game section

--- src/app/page.js
+++ src/app/page.js
@@ -2,11 +2,13 @@ import Hero from "@/views/sections/Hero";
 import PerformanceStats from "@/views/sections/PerformanceStats";
 import WorkWithUsSection from "@/views/sections/WorkWithUs";
 import StartEarning from "@/views/sections/startEarning";
+import Ourgames from "@/views/sections/ourgames";
 export default function Home() {
   return (
     <main>
       <Hero />
       <PerformanceStats />
+      <Ourgames />
       <WorkWithUsSection />
       <StartEarning />
     </main>
---
+++ src/views/sections/ourgames.js
@@ -0,0 +1,59 @@
+import React from "react";
+import Image from "next/image";
+const Ourgames = () => {
+  return (
+    <section className="xxl-max-screen w-full flex flex-col">
+      <div className="w-full flex gap-4">
+        {[
+          "/images/game1.svg",
+          "/images/game2.png",
+          "/images/game3.svg",
+          "/images/game4.svg",
+          "/images/game5.svg",
+        ]?.map((item, index) => (
+          <Image
+            key={index}
+            src={item}
+            alt="Our game"
+            width={276}
+            height={458}
+          />
+        ))}
+      </div>
+      <div className="w-full flex justify-between">
+        <Image
+          src={"/images/game4.svg"}
+          alt="Our game"
+          width={276}
+          height={458}
+        />
+        <div></div>
+        <Image
+          src={"/images/game4.svg"}
+          alt="Our game"
+          width={276}
+          height={458}
+        />
+      </div>
+
+      <div className=" w-full flex gap-4">
+        {[
+          "/images/game1.svg",
+          "/images/game2.png",
+          "/images/game3.svg",
+          "/images/game4.svg",
+          "/images/game5.svg",
+        ]?.map((item, index) => (
+          <Image
+            key={index}
+            src={item}
+            alt="Our game"
+            width={276}
+            height={458}
+          />
+        ))}
+      </div>
+    </section>
+  );
+};
+export default Ourgames;
