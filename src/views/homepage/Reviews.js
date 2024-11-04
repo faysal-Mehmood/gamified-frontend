78e9dfd983ddb6aeebef1278f0ddea9cc4dcac63
fasyal-ext <faysal.ext@ahdustechnology.com>
2024-11-04 23:27:09 +0500

    fix more on review

--- src/views/homepage/Reviews.js
+++ src/views/homepage/Reviews.js
@@ -123,7 +123,7 @@ const Circle = ({ fitBackColor, currentSlide, index }) => {
     <div className={`w-fit h-fit relative  `}>
       {/* Top circle */}
       <div
-        className={`absolute z-1 rounded-full border-2 border-brand-blue 
+        className={`absolute z-1 rounded-full border-2 border-brand-blue backdrop-blur-[10px] overflow-hidden
       bg-circle-gradient ${
         fitBackColor ? "translate-y-[90px] translate-x-[200px]" : ""
       }
@@ -195,7 +195,7 @@ const Circle = ({ fitBackColor, currentSlide, index }) => {
 
       {/* Bottom circle */}
       <div
-        className={`absolute z-1 rounded-full border-2 border-brand-blue 
+        className={`absolute z-1 rounded-full border-2 border-brand-blue backdrop-blur-[10px] overflow-hidden
       bg-circle-gradient ${
         fitBackColor ? "translate-y-[-170px] translate-x-[5rem]" : ""
       }
