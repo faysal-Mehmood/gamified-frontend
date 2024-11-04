48ac0053e500908362541b257ee5944f5057f52a
faysal-Mehmood <faysal.mehmood101@gmail.com>
2024-11-04 18:25:19 +0500

    fix more on deashboard

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,7 +1,11 @@
 import React from "react";
 import BalanceOverview from "@/views/dashboard/balanceOverview";
 const page = () => {
-  return <BalanceOverview />;
+  return (
+    <div className="dashboard_body">
+      <BalanceOverview />
+    </div>
+  );
 };
 
 export default page;
