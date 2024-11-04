2740b611559500aeeeafb22497e171a74c6215c4
fasyal-ext <faysal.ext@ahdustechnology.com>
2024-11-04 22:42:54 +0500

    fix mroe

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,11 +1,7 @@
 import React from "react";
 import BalanceOverview from "@/views/dashboard/balanceOverview";
 const page = () => {
-  return (
-    <div className="dashboard_body">
-      <BalanceOverview />
-    </div>
-  );
+  return <BalanceOverview />;
 };
 
 export default page;
